e14b768ba38b625386c24f6c52d6e0e433ecea3f
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-11-05 12:38:30 -0500

    updated Today Component

--- client/src/components/TodayTasks/index.js
+++ client/src/components/TodayTasks/index.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "../common/Styles/commonStyles.css";
+import Form from "../common/Form";
 
 const index = () => {
-  return <div>This is TodayTask Component!</div>;
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <Form />
+      </div>
+    </div>
+  );
 };
 
 export default index;

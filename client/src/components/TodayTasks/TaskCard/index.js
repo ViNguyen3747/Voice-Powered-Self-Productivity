d65b58a03ea6aa7c4a83af43a9f6eb27974a53a4
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-11-07 00:29:23 -0500

    add Task Card

---
+++ client/src/components/TodayTasks/TaskCard/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const index = (task) => {
+  console.log(task);
+  return <div></div>;
+};
+
+export default index;
--- client/src/components/TodayTasks/index.js
+++ client/src/components/TodayTasks/index.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "../common/Styles/commonStyles.css";
-import Form from "../common/Form";
 import { gql, useQuery } from "@apollo/client";
 
+import "../common/Styles/commonStyles.css";
+import "./Today.css";
+import Form from "../common/Form";
+import TaskCard from "./TaskCard";
 const GET_TASKS = gql`
   query Tasks {
     tasks {
+      id
       name
       category
       priorityLevel
@@ -17,10 +20,16 @@ const GET_TASKS = gql`
 const Today = () => {
   const { loading, error, data } = useQuery(GET_TASKS);
   console.log(data);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>There was an error</div>;
   return (
     <div className="container">
       <div className="wrapper">
         <Form />
+      </div>
+      <div className="tasks-container">
+        {data &&
+          data.tasks.map((task) => <TaskCard key={task.id} task={task} />)}
       </div>
     </div>
   );

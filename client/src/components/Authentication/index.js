eadccb349bb13b209bd70233f80072376bb5cb35
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-11-06 23:46:14 -0500

    update Auth style

--- client/src/components/Authentication/index.js
+++ client/src/components/Authentication/index.js
@@ -1,28 +1,23 @@
 import React from "react";
-import "../common/Styles/commonStyles.css";
-import Register from "./registration"
-import Sinin from './signin'
-import {BrowserRouter as Router, Route, Switch,} from "react-router-dom"
-const Index = ()=>{
-
-
+import Register from "./registration";
+import Sinin from "./signin";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./Auth.css";
+const Index = () => {
   return (
-      <div>
-          <Router>
+    <div>
+      <Router>
         <Switch>
-            <Route exact path="/">
-                <Register/>
-            </Route>
-            <Route>
-                <Sinin/>
-            </Route>
+          <Route exact path="/">
+            <Register />
+          </Route>
+          <Route>
+            <Sinin />
+          </Route>
         </Switch>
-    </Router>
-      </div>
-    
-    
+      </Router>
+    </div>
   );
-}
-
+};
 
 export default Index;

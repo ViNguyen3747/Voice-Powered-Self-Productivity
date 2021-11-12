ae5a47ed006fe998ebe6b1d57872be7e2a443ac9
Long Le <longle@Longs-MacBook-Pro.local>
2021-11-12 13:45:54 -0600

    update query util

--- client/src/utils/query.js
+++ client/src/utils/query.js
@@ -2,14 +2,28 @@
 import { gql } from "@apollo/client";
 
 export const GET_TASKS = gql`
-  query Tasks {
-    tasks {
+  query Tasks($category: Category) {
+    tasks(category: $category) {
       id
       name
       category
       priorityLevel
       duration
       isDone
+      createdAt
+    }
+  }
+`;
+export const GET_TASK = gql`
+  query Task($taskId: ID!) {
+    task(id: $taskId) {
+      name
+      id
+      category
+      priorityLevel
+      duration
+      isDone
+      createdAt
     }
   }
 `;

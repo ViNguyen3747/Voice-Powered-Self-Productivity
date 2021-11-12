459968c7fbf8136dc628c4109a8864198880cfee
Thi Kieu Vi Nguyen <vinguyen1172000@gmail.com>
2021-11-12 09:29:00 -0600

    updated utils

--- client/src/utils/query.js
+++ client/src/utils/query.js
@@ -1,1 +1,15 @@
 //complete this file
+import { gql, useQuery } from "@apollo/client";
+
+export const GET_TASKS = gql`
+  query Tasks {
+    tasks {
+      id
+      name
+      category
+      priorityLevel
+      duration
+      isDone
+    }
+  }
+`;

32197c35eeba16e51a08aa7170e8ebe28fcbf71a
Long Le <longle@Longs-MacBook-Pro.local>
2021-11-15 09:47:07 -0600

    add update and delete for mutation utils

--- client/src/utils/mutation.js
+++ client/src/utils/mutation.js
@@ -1,1 +1,33 @@
 //complete this file
+import { gql } from "@apollo/client";
+
+export const UPDATE_TASK = gql`
+  mutation UpdateTask($updateTaskId: ID!, $input: updateTaskInput) {
+    updateTask(id: $updateTaskId, input: $input) {
+      id
+      name
+      category
+      priorityLevel
+      duration
+      isDone
+      date
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const DELETE_TASK = gql`
+  mutation DeleteTask($deleteTaskId: ID!) {
+    deleteTask(id: $deleteTaskId) {
+      id
+      name
+      category
+      priorityLevel
+      isDone
+      duration
+      date
+      createdAt
+      updatedAt
+    }
+  }
+`;

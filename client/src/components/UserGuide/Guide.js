5dbdf74ee4ea66935782a695bebcf29ecf702286
Ombeni Nusra <onusra@angelo.edu>
2021-12-10 10:53:24 -0600

    user guide

--- client/src/components/UserGuide/Guide.js
+++ client/src/components/UserGuide/Guide.js
@@ -1,26 +1,38 @@
 import React from "react";
 import  "../common/Styles/commonStyles.css"
-import image from "../images/categories/emotional.svg"
+import Sinupimage from "../images/useGuide/signup.png"
+import deleteImage from "../images/useGuide/delete.png"
+import taskImage from "../images/useGuide/task.png"
+import voiceImage from "../images/useGuide/voice.png"
 
 const userGuide = [
   {
     value:
     "To use the App you must register for an acount by filling up the join us page wich can be accessed by clicking the join us link bellow or by Sing in if you already have an account",
-    image: image,
+    image: Sinupimage,
     color: "#6D9773",
+  },
+
+  {
+    value:
+      "To create tasks, fill in name of the task, task category, the priority level of the task, the task date, the task starting and finishing time",
+    color: "#6D9773",
+    image: taskImage,
   },
   {
     value:
-      "To ",
-      image: image ,
+      "To update, edit or delete tasks use the the pencil symbol for edit and a trash symbol for delete",
+      image: deleteImage ,
     color: "#FFBA00",
   },
+
   {
     value:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
-    color: "#6D9773",
-    image: image,
+      "The app is Voice powered, you can enter and edit tasks using voice by holding the microphone",
+      image: voiceImage ,
+    color: "#FFBA00",
   },
+  
 ];
 export const Guide = () => {
   return (
@@ -28,7 +40,7 @@ export const Guide = () => {
       <div className="title" style={{ color: "white" }}>
         User Guides
       </div>
-      <div className="info-wrapper">
+      <div className="guide-wrapper">
         {userGuide.map((g) => (
           <div
             className="guideCard"
@@ -40,8 +52,8 @@ export const Guide = () => {
             <div>
              {g.value}
             </div>
-            <div>
-            <img className='userGuideimage' src={g.image} alt=''/>
+            <div className='userGuideimage'>
+            <img width="500px" src={g.image} alt=''/>
             </div>
           </div>
         ))}

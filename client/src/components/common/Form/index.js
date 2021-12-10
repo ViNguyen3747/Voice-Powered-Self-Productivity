0ec437f17723d85662eb9ab225676ba843fc74c9
longle38 <84715680+longle38@users.noreply.github.com>
2021-12-09 20:04:03 -0600

    update Form

--- client/src/components/common/Form/index.js
+++ client/src/components/common/Form/index.js
@@ -1,13 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { animateScroll as scroll } from "react-scroll";
+import { Controller, useForm } from "react-hook-form";
 import { useSpeechContext } from "@speechly/react-client";
 import { useMutation, useQuery } from "@apollo/client";
-import { Form, Button, Grid, Label } from "semantic-ui-react";
+import { Form, Button, Grid, Label, Typo } from "semantic-ui-react";
 import SemanticDatepicker from "react-semantic-ui-datepickers";
 import "./form.css";
 import "../../common/Styles/commonStyles.css";
 import { categoriesOptions, priorityOptions } from "../Data";
 import { ADD_TASK, UPDATE_TASK } from "../../../utils/graphQL/mutation";
 import { GET_TASK } from "../../../utils/graphQL/query";
+import { taskSchema } from "../../../utils/validation/taskFormValidation";
 const LabelTag = ({ text }) => (
   <div className="label">
     <Label tag color="black" size="large">
@@ -18,19 +22,30 @@ const LabelTag = ({ text }) => (
 
 const initialState = {
   name: "",
-  category: "",
-  prioritylevel: "",
-  duration: 0,
+  category: "Professional",
+  prioritylevel: "A",
+  duration: -1,
   isDone: false,
+  start: "00:00",
+  finish: "00:00",
   date: new Date(),
 };
 
-const initialTime = { start: "00:00", finish: "00:00" };
-
 const TaskForm = ({ currentId, setCurrentId, rerouting }) => {
+  const {
+    handleSubmit,
+    clearErrors,
+    register,
+    setValue,
+    getValues,
+    control,
+    formState: { errors },
+    reset,
+  } = useForm({
+    resolver: yupResolver(taskSchema),
+    defaultValues: initialState,
+  });
   const { segment } = useSpeechContext();
-  const [formState, setFormState] = useState(initialState);
-  const [timerange, setTime] = useState(initialTime);
   const {
     data,
     loading,
@@ -40,75 +55,61 @@ const TaskForm = ({ currentId, setCurrentId, rerouting }) => {
   const [addTask, { error }] = useMutation(ADD_TASK);
   const [updateTask, { error: updateError }] = useMutation(UPDATE_TASK);
 
-  const handleChange = (event, { name, value, type }) => {
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-    console.log(formState);
-  };
-
   useEffect(() => {
     if (data) {
-      console.log(data);
-      let { name, category, prioritylevel, duration, date, start, finish } =
-        data.task;
-      setFormState({
-        name,
-        category,
-        prioritylevel,
-        duration,
-        date: new Date(date),
-      });
-      setTime({ start, finish });
+      clearErrors();
+      scroll.scrollToTop();
+
+      let { createdAt, id, isDone, owner, ...task } = data.task;
+
+      Object.entries(task).map(([key, value]) =>
+        setValue(key, key === "date" ? new Date(value) : value)
+      );
     }
 
     if (segment) {
-      if (segment.isFinal) {
-        if (segment.intent.intent === "create") {
-          setCurrentId(null);
-          setFormState(initialState);
-          segment.entities.forEach((e) => {
-            if (e.type !== "start" || e.type !== "finish") {
-              setFormState({ ...formState, [e.type]: e.value });
-            } else {
-              setTime({ ...timerange, [e.type]: e.value });
-            }
-          });
-        } else if (segment.intent.intent.includes("update")) {
-          let intent = segment.intent.intent.replace("update_", "");
-          if (currentId) {
-            if (intent !== "duration") {
-              setFormState({
-                ...formState,
-                [segment.entities[0].type]: segment.entities[0].value,
-              });
-            } else {
-              segment.entities.forEach((e) => {
-                setTime({ ...timerange, [e.type]: e.value });
-              });
-            }
-          }
-        }
+      reset();
+      if (segment.intent.intent === "create") {
+        setCurrentId(null);
+      }
+      segment.entities.forEach((e) => {
+        console.log(e);
+        let value = e.value;
+        if (e.type === "date") value = new Date(e.value);
+        if (e.type === "name") value = e.value.toLowerCase();
+        if (e.type === "category")
+          value = e.value.charAt(0) + e.value.substring(1).toLowerCase();
+        setValue(e.type, value);
+      });
+      if (
+        segment.isFinal &&
+        getValues("name") &&
+        getValues("category") &&
+        getValues("prioritylevel") &&
+        getValues("date") &&
+        getValues("start") &&
+        getValues("finish")
+      ) {
         handleFormSubmit();
       }
     }
   }, [data, segment]);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+  const handleFormSubmit = async () => {
+    setValue(
+      "duration",
+      parseFloat(
+        timeConvert(getValues("finish")) - timeConvert(getValues("start"))
+      )
+    );
+    let taskData = getValues();
     try {
       if (currentId) {
         const { data } = await updateTask({
           variables: {
             updateTaskId: currentId,
             input: {
-              ...formState,
-              start: timerange.start,
-              finish: timerange.finish,
-              duration: parseFloat(
-                timeConvert(timerange.finish) - timeConvert(timerange.start)
-              ),
+              ...taskData,
             },
           },
         });
@@ -117,12 +118,7 @@ const TaskForm = ({ currentId, setCurrentId, rerouting }) => {
         const { data } = await addTask({
           variables: {
             input: {
-              ...formState,
-              start: timerange.start,
-              finish: timerange.finish,
-              duration: parseFloat(
-                timeConvert(timerange.finish) - timeConvert(timerange.start)
-              ),
+              ...taskData,
               isDone: false,
             },
           },
@@ -134,15 +130,9 @@ const TaskForm = ({ currentId, setCurrentId, rerouting }) => {
     }
   };
   const clear = () => {
-    setFormState(initialState);
+    reset();
     setCurrentId(null);
-    setTime(initialTime);
-  };
-  const timeChange = (event, { name, value }) => {
-    setTime({
-      ...timerange,
-      [name]: value,
-    });
+    clearErrors();
   };
 
   const timeConvert = (time) => {
@@ -150,75 +140,92 @@ const TaskForm = ({ currentId, setCurrentId, rerouting }) => {
     return +t[0] * 60 * 60 + +t[1] * 60;
   };
   return (
-    <Form className="form-container">
+    <Form className="form-container" onSubmit={handleSubmit(handleFormSubmit)}>
       <Grid>
-        <Grid.Column mobile={16} tablet={8} computer={8}>
-          <LabelTag text="Task Name" />
-          <Form.Input
-            name="name"
-            onChange={handleChange}
-            value={formState.name}
-            placeholder="Task Name"
-          />
+        <Grid.Column mobile={16} tablet={16} computer={16}>
+          {segment && segment.words.map((w) => w.value).join(" ")}
         </Grid.Column>
         <Grid.Column mobile={16} tablet={8} computer={8}>
-          <LabelTag text="Category" />
-          <Form.Select
-            options={categoriesOptions}
-            name="category"
-            onChange={handleChange}
-            value={formState.category}
-            placeholder="Category"
-          />
+          <Form.Field required>
+            <LabelTag text="Task Name" />
+            <input type="text" placeholder="task name" {...register("name")} />
+            <p className="error">{errors.name?.message}</p>
+          </Form.Field>
+        </Grid.Column>
+        <Grid.Column mobile={16} tablet={8} computer={8}>
+          <Form.Field>
+            <LabelTag text="Category" />
+            <Controller
+              control={control}
+              name="category"
+              render={({ field: { onChange } }) => (
+                <>
+                  <Form.Select
+                    options={categoriesOptions}
+                    onChange={(event, data) => onChange(data.value)}
+                    value={getValues("category")}
+                    placeholder="category"
+                  />
+                  <p className="error">{errors.category?.message}</p>
+                </>
+              )}
+            />
+          </Form.Field>
         </Grid.Column>
 
         <Grid.Column mobile={16} tablet={8} computer={8}>
-          <div>
+          <Form.Field>
             <LabelTag text="Priority Level" />
-            <Form.Select
-              options={priorityOptions}
+            <Controller
+              control={control}
               name="prioritylevel"
-              onChange={handleChange}
-              value={formState.prioritylevel}
-              placeholder="priority level"
+              render={({ field: { onChange, value } }) => (
+                <>
+                  <Form.Select
+                    options={priorityOptions}
+                    value={getValues("prioritylevel")}
+                    onChange={(event, data) => onChange(data.value)}
+                    placeholder="priority level"
+                  />
+                  <p className="error">{errors.prioritylevel?.message}</p>
+                </>
+              )}
             />
-          </div>
+          </Form.Field>
           <div className="button-container">
-            <LabelTag text="Date" />
-            <SemanticDatepicker
-              size="large"
-              name="date"
-              onChange={handleChange}
-              value={formState.date}
-            />
+            <Form.Field>
+              <LabelTag text="Date" />
+              <Controller
+                control={control}
+                name="date"
+                render={({ field }) => (
+                  <>
+                    <SemanticDatepicker
+                      value={getValues("date")}
+                      onChange={(event, data) => field.onChange(data.value)}
+                    />
+                    <p className="error">{errors.date?.message}</p>
+                  </>
+                )}
+              />
+            </Form.Field>
           </div>
         </Grid.Column>
 
         <Grid.Column mobile={8} tablet={8} computer={8}>
-          <>
-            <LabelTag text="Start" />
-            <Form.Input
-              size="small"
-              type="time"
-              name="start"
-              value={timerange.start}
-              onChange={timeChange}
-            />
-          </>
-          <>
-            <LabelTag text="Finish" />
-            <Form.Input
-              size="small"
-              type="time"
-              name="finish"
-              value={timerange.finish}
-              onChange={timeChange}
-            />
-          </>
+          <LabelTag text="Start" />
+          <Form.Field size="small">
+            <input type="time" name="start" {...register("start")} />
+          </Form.Field>
+          <LabelTag text="Finish" />
+          <Form.Field size="small">
+            <input type="time" name="finish" {...register("finish")} />
+            <p className="error">{errors.finish?.message}</p>
+          </Form.Field>
         </Grid.Column>
 
         <Grid.Column mobile={16} tablet={16} computer={16}>
-          <Button color="black" size="big" fluid onClick={handleFormSubmit}>
+          <Button color="black" size="big" fluid type="submit">
             Submit
           </Button>
         </Grid.Column>
